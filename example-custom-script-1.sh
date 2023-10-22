a7e552076bc9991a880bdf8b6ddb056a5bd444db
2279244533 <131430121+2279244533@users.noreply.github.com>
2023-10-22 10:29:55 +0800

    Update example-custom-script-1.sh

--- example-custom-script-1.sh
+++ example-custom-script-1.sh
@@ -1,38 +1,20 @@
 #!/bin/bash
-# Copyright (c) 2022-2023 Curious <https://www.curious.host>
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-# 
-# https://github.com/Curious-r/OpenWrtBuildWorkflows
-# Description: Automatically check OpenWrt source code update and build it. No additional keys are required.
-#-------------------------------------------------------------------------------------------------------
 #
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# This script will run before feeds update, something you want to do at that moment should be written here.
-# A common function of this script is to modify the cloned OpenWrt source code. 
-#
-# For instance, you can edit the feeds.conf.default to induct packages you need.
-# This is followed by some editing examples.
-# # Clear the feeds.conf.default and append the feed sources you need one by one:
-#cat /dev/null > a.txt
-#echo 'src-git-full packages https://git.openwrt.org/feed/packages.git;openwrt-22.03' >> feeds.conf.default
-#echo 'src-git-full luci https://git.openwrt.org/project/luci.git;openwrt-22.03' >> feeds.conf.default
-#echo 'src-git-full routing https://git.openwrt.org/feed/routing.git;openwrt-22.03' >> feeds.conf.default
-#echo 'src-git-full telephony https://git.openwrt.org/feed/telephony.git;openwrt-22.03' >> feeds.conf.default
-# # Replace a feed source with what you want:
-#sed '/feeds-name/'d feeds.conf.default
-#echo 'method feed-name path/URL' >> feeds.conf.default
-# # Uncomment a feed source:
-#sed -i 's/^#\(.*feed-name\)/\1/' feeds.conf.default
-# # Replace src-git-full with src-git to reduce the depth of cloning:
-#sed -i 's/src-git-full/src-git/g' feeds.conf.default
-#
-# You can also modify the source code by patching.
-# # Here's a template for patching:
-#touch example.patch
-#cat>example.patch<<EOF
-#patch content
-#EOF
-#git apply example.patch
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
+git clone https://github.com/Zxilly/UA2F package/UA2F
 
